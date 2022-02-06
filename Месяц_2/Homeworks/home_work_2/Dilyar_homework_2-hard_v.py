42dea812eb423a3d2e3974a2d1a48d5d2ffa8b4b
Dilyar0 <diliartemirbekov01@gmail.com>
2022-02-06 14:59:21 +0600

    debag cod

--- Месяц_2/Homeworks/home_work_2/Dilyar_homework_2-hard_v.py
+++ Месяц_2/Homeworks/home_work_2/Dilyar_homework_2-hard_v.py
@@ -1,29 +1,19 @@
 # home work hard version
 class Figure:
-    unit = 50
-    def __init__(self, cm):
+    unit = "cm"
+    def __init__(self):
         self.__calculate_perimeter()
         self.calculate_area()
-        self.unit = cm
-
-    def perimeter(self, __perimeter=0):
-        self.premieter = __perimeter
 
     def get_perimeter(self):
-        return self.perimeter
-
+        return self.__perimeter
     def set_perimeter(self, perimeter):
-        self.perimeter = perimeter
-
-    def __str__(self):
-        pass
+        self.__perimeter = perimeter
 
     def calculate_area(self):
-        print("Calculate Area")
-
+        pass
     def __calculate_perimeter(self):
-        print("Calculate Perimeter")
-
+        pass
     def info(self):
         pass
 
@@ -34,15 +24,16 @@ class Square(Figure):
         self.perimeter = self.__calculate_perimeter()
 
     def calculate_area(self):
-        return f"Square: {self.__side_length * 2}"
+        return self.__side_length * 2
 
     def __calculate_perimeter(self):
-        return f"Perimeter: {self.__side_length * 4}"
+        return self.__side_length * 4
 
     def info(self):
-        print(f"SQUARE: Side length:{self.__side_length} Area:{self.calculate_area()} Perimeter:{self.__calculate_perimeter()}")
+        print(f"Square:   Side length:{self.__side_length}{self.unit}   Perimeter:{self.__calculate_perimeter()}{self.unit}   Area:{self.calculate_area()}{self.unit}")
 
 class Rectangle(Figure):
+
     def __init__(self, __length, __width):
         self.__width = __width
         self.__length = __length
@@ -54,21 +45,16 @@ class Rectangle(Figure):
         return (self.__length + self.__width) * 2
 
     def info(self):
-        print(f"RECTANGLE: Length:{self.__length}cm Width:{self.__width}cm Perimeter:{self.__calculate_perimeter()}cm Area:{self.calculate_area()}cm ")
+        print(
+            f"Rectangle: Length:{self.__length}{self.unit}   Width:{self.__width}{self.unit}   Perimeter:{self.__calculate_perimeter()}{self.unit}   Area:{self.calculate_area()}{self.unit}")
 
-square1 = Square(5)
-square2 = Square(9)
+sq1 = Square(1)
+sq2 = Square(3)
 
-rectengle1 = Rectangle(1, 3)
-rectengle2 = Rectangle(7, 9)
-rectengle3 = Rectangle(4, 8)
+rec1 = Rectangle(5, 8)
+rec2 = Rectangle(1, 3)
+rec3 = Rectangle(1, 4)
 
-
-rectengles = [rectengle1, rectengle2, rectengle3]
-squares = [square1, square2]
-
-for i in rectengles:
+general = [sq1, sq2, rec1, rec2, rec3]
+for i in general:
     i.info()
-
-for k in squares:
-    k.info()
--- Месяц_2/Homeworks/home_work_2/Dilyar_homework_2_llite_v.py
+++ Месяц_2/Homeworks/home_work_2/Dilyar_homework_2_llite_v.py
@@ -1,12 +1,12 @@
 # home work lite version
 class Figure:
-    unit = None
+    unit = "cm"
     def __init__(self):
-        self.unit = "mm"
-        def calculate_area(self):
-            pass
-        def info(self):
-            pass
+        pass
+    def calculate_area(self):
+        pass
+    def info(self):
+        pass
 
 class Circle(Figure):
     p = 3.14
@@ -15,7 +15,7 @@ class Circle(Figure):
     def calculate_area(self):
         return Circle.p * (self.__radius ** 2)
     def info(self):
-        print(f"Circle radius: {self.__radius}, area: {self.calculate_area}cm")
+        print(f"Circle radius: {self.__radius}, area: {self.calculate_area()} {self.unit}")
 
 class Triangle(Figure):
     def __init__(self, side_a,side_b):
@@ -25,7 +25,7 @@ class Triangle(Figure):
     def calculate_area(self):
         return self.__side_b * self.__side_a / 2
     def info(self):
-        print(f"Triangle side a: {self.__side_a}, side b: {self.__side_b}, area: {self.calculate_area()}.")
+        print(f"Triangle side a: {self.__side_a}, side b: {self.__side_b}, area: {self.calculate_area()} {self.unit}.")
 
 ciOne = Circle(8)
 ciTwo = Circle(4)
@@ -34,10 +34,6 @@ trOne = Triangle(3, 6)
 trTwo = Triangle(4, 9)
 trTree = Triangle(2, 4)
 
-circles = [ciOne, ciTwo]
-triangles = [trOne, trTwo, trTree]
-
-for i in circles:
+general = [ciOne, ciTwo, trOne, trTwo, trTree]
+for i in general:
     i.info()
-for j in triangles:
-    j.info()
