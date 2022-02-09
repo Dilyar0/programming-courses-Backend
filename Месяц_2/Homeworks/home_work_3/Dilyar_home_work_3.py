c4d9da495384280d89f5c6a304b07947acd6538b
Dilyar0 <diliartemirbekov01@gmail.com>
2022-02-09 11:08:28 +0600

    version-2

--- Месяц_2/Homeworks/home_work_3/Dilyar_home_work_3.py
+++ Месяц_2/Homeworks/home_work_3/Dilyar_home_work_3.py
@@ -20,16 +20,14 @@ class Computer:
         self.__memory = value
 
     def make_computations(self):
-        return f" CPU + Memory = {self.__cpu + self.__memory}"
+        return f"CPU + Memory = {self.__cpu + self.__memory}"
 
     def __str__(self):
         return f"CPU: {self.__cpu} " \
                f"Memory: {self.__memory}"
 
     def __gt__(self, other):
-        return self.memory >= other.memory, \
-               self.memory <= other.memory, \
-               self.memory == other.memory
+        return self.memory > other.memory
 
 
 class Phone:
@@ -41,22 +39,20 @@ class Phone:
         return self.__sim_cards_list
 
     @sim_cards_list.setter
-    def sim_cards_list(self, *sim_cards_list):
-        self.__sim_cards_list = sim_cards_list
+    def sim_cards_list(self, value):
+        self.__sim_cards_list = value
 
     def call(self, sim_card_number, call_to_number):
-        print(f"Идет звонок на номер {call_to_number} с сим-карты-{sim_card_number}, {self.__sim_cards_list[sim_card_number-1]}")
+        print(f"Идет звонок на номер {sim} с сим-карты-{sim}")
 
     def __str__(self):
         return f"sim cards list: {self.__sim_cards_list}"
 
 
 class SmartPhone(Computer, Phone):
-    def __init__(self, cpu, memory, location, *sim_cards_list):
+    def __init__(self, cpu, memory, *sim_cards_list):
         Computer.__init__(self, cpu, memory)
         Phone.__init__(self, sim_cards_list)
-        sim_cards_list = list(sim_cards_list)
-        self.location = location
 
     def use_gps(self, location):
         print(f"Локация отмечена, ведется маршрут в {location}")
@@ -66,14 +62,19 @@ class SmartPhone(Computer, Phone):
                f"Memory:{self.memory} " \
                f"Sim card: {self.sim_cards_list}"
 
-
+sim = ["1 - O!", "2 - Beeline"]
 pc = Computer(3500, 8)
+pcInfo = Computer.make_computations(pc)
 tel = Phone(1)
-smart = SmartPhone(888, 6, "", "+996 500 12 34 56")
-smart2 = SmartPhone(2500, 4, "", "+996 555 78 94 56")
+smartP = SmartPhone(888, 6, sim[0])
+smartP2 = SmartPhone(2500, 4, sim[1])
+smartP.call("+996 500 12 34 56", sim[0])
+smartP.use_gps("Аламедин")
+
 
 print(pc)
 print(tel)
-print(smart)
-print(smart2)
-print(SmartPhone.use_gps(" ","Alameidin"))
+print(pcInfo)
+print(smartP)
+print(smartP2)
+print(pc > smartP)
