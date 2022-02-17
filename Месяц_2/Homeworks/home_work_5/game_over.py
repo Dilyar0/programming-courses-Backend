78abd44cdac7e048d565b613f801d056fc6ac051
Dilyar0 <diliartemirbekov01@gmail.com>
2022-02-17 12:55:42 +0600

    .

---
+++ Месяц_2/Homeworks/home_work_5/game_over.py
@@ -0,0 +1,2 @@
+from main import Casino
+winner = Casino()
--- Месяц_2/Homeworks/home_work_5/main.py
+++ Месяц_2/Homeworks/home_work_5/main.py
@@ -4,46 +4,59 @@ from envparse import env
 
 class Casino:
     def __init__(self):
-        self.cash = int(os.environ.get('MY_MONEY'))
+        self.__cash = int(os.environ.get('MY_MONEY'))
 
-        while int(self.cash) != 0:
-            print(f"Ваши деньги: {self.cash}$")
-            self.win_slot = random.randint(1, 30)
-            self.slot = range(1, 31)
-            self.bet = int(input('Сделайте ставку: '))
-
-            if self.bet > self.cash:
-                    print("У вас недостаточно денег")
+        while int(self.__cash) > 0:
+            print (f"Ваш денги: {self.__cash}")
+            self.__win_slot = random.randint(1, 30)
+            print(self.__win_slot)
+            self.__slot = int(30)
+            self.__bet = int(input('Сделайте ставку: '))
+            if self.__bet > self.__cash:
+                print("У вас недостаточно денег! ")
             else:
                 self.__choice = int(input('Выберите слот: '))
-                if self.__choice > range:
+                if self.__choice > self.__slot:
                     print("Выберите слота только от 1-30")
-                    repeat = input('Хотите ешё поиграть? ("да" или "нет"): ')
-                    if repeat == 'да':
+                    userRound = input('Хотите ешё поиграть?("да" или "нет"')
+                    if userRound == 'да':
                         continue
-                    elif repeat == "нет":
+                    elif userRound == 'нет':
                         break
                     else:
-                        continue
-                        print('"да" или "нет"?')
+                        print("да или нет?")
 
-                if self.win_slot == self.__choice:
-                    self.cash += self.bet
-                    print(f'You win\n money: {self.cash}')
-                    if self.win_slot == self.__choice:
+                if self.__win_slot == self.__choice:
+                    self.__cash += self.__bet
+                    print(f'You win\n money: {self.__cash}')
+                    if self.__win_slot == self.__choice:
                         ans = input('do you want to play more?(yes or write any words): ')
                         if ans != 'yes':
                             break
 
                 else:
-                    self.cash -= self.bet
-                    print(f' You lost \n money: {self.cash}')
-                    if self.win_slot != self.choice:
+                    self.__cash -= self.__bet
+                    print(f' You lost \n money: {self.__cash}')
+                    if self.__win_slot != self.__choice:
                         ans = input('do you want to play again?(yes or write any words): ')
                         if ans != 'yes':
                             break
 
+    @property
+    def win(self):
+        return self.__win_slot
 
+    @win.setter
+    def win(self, value):
+        self.__win_slot = value
+
+    @property
+    def slot(self):
+        return self.__slot
+
+    @slot.setter
+    def slot(self, value):
+        self.__slot = value
 
 
 env.read_envfile('settings.env')
