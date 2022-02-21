97c6430971494843a0d34b363f4ce1898535f294
Dilyar0 <diliartemirbekov01@gmail.com>
2022-02-21 09:39:16 +0600

    home_work_6

--- Месяц_2/Homeworks/home_work_6/home_work_6.py
+++ Месяц_2/Homeworks/home_work_6/home_work_6.py
@@ -1,5 +1,5 @@
-class Date:
-    def __init__(self, full_name, email,  file_name, color):
+class Data:
+    def __init__(self, full_name, email, file_name, color):
         self.__full_name = full_name
         self.__email = email
         self.__file_name = file_name
@@ -37,3 +37,44 @@ class Date:
     def color(self, value):
         self.__color = value
 
+
+files = ['full_name.txt', 'email.txt', 'file_name.txt', 'color.txt']
+
+
+def MainFunc():
+    with open('MOCK_DATE.txt') as readOn:
+        for i in readOn:
+            stringM = i.split()
+            stringM[0] += ' ' + stringM.pop(1)
+            if len(stringM) > 4:
+                stringM[0] += ' ' + stringM.pop(1)
+            info = Data(*stringM)
+            with open(files[0], 'a') as f0:
+                f0.write(info.full_name)
+                f0.write('\r')
+            with open(files[1], 'a') as f1:
+                f1.write(info.email)
+                f1.write('\r')
+            with open(files[2], 'a') as f2:
+                f2.write(info.file_name)
+                f2.write('\r')
+            with open(files[3], 'a') as f3:
+                f3.write(info.color)
+                f3.write('\r')
+
+def RemList():
+    file = open('dates/full_name.txt', 'w')
+    file.close()
+
+    file = open('dates/email.txt', 'w')
+    file.close()
+
+    file = open('dates/file_name.txt', 'w')
+    file.close()
+
+    file = open('dates/color.txt', 'w')
+    file.close()
+
+
+RemList()
+MainFunc()
